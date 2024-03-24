4e33ac3a1bcafe17b280c54b9dc71e32210c212f
Katarya Johnson-Williams <kajohnsonwilliam3168@eagle.fgcu.edu>
2024-03-24 19:06:25 -0400

    Added base code to assigned classes.

--- src/ShoulderandElbow.java
+++ src/ShoulderandElbow.java
@@ -7,8 +7,53 @@ File: ShoulderandElbow.java
 Description: Class to test the Shoulder & Elbow page on Nano Experience website.
  */
 
+// Selenium Import Statements
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+// TestNG Import Statements
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 public class ShoulderandElbow {
+    // Protected variable to store the webdriver
+    protected WebDriver driver;
 
+    // Store website URL in a string variable for easy access
+    protected String ShoulderAndElbowURL = "https://www.thenanoexperience.com/shoulder-and-elbow";
+
+    // Set up the webdriver before we start running the class
+    @BeforeClass
+    public void setUp() throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "WebDrivers/Chromedriver/chromedriver.exe");
+        driver = new ChromeDriver();
+
+        // Open and maximize the Nano Experience page
+        driver.get(ShoulderAndElbowURL);
+        driver.manage().window().maximize();
+        Thread.sleep(3000);
+    }
+
+    // Close the webdriver after we are done running the class
+    @AfterClass
+    public void close() {
+        driver.quit();
+    }
+
+    // ---------------------- Test Functions ------------------------------------
+    @Test(priority = 1)
+    void url_and_title() throws InterruptedException {
+        // Get and print URL of webpage
+        String pageURL = driver.getCurrentUrl();
+        System.out.print("Current Page URL: ");
+        System.out.print(pageURL);
+        System.out.println();
+
+        // Get and print title of webpage
+        String title = driver.getTitle();
+        System.out.print("Current Page Title: ");
+        System.out.print(title);
+        System.out.println();
+    }
 }
