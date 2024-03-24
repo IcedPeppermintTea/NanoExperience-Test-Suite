af0d5b7dbe65b9ed942578808512e2e278218a12
Ayleen Roque <amroque8479@eagle.fgcu.edu>
2024-03-24 15:41:45 -0400

    Added BeforeClass and AfterClass

--- src/Navbar.java
+++ src/Navbar.java
@@ -8,10 +8,29 @@ Description:
  */
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class Navbar
 {
+    // Variable to store the webdriver
+    protected WebDriver driver;
+
+    // Set up the webdriver before we start running the class
+    @BeforeClass // set up the webdriver before we begin testing
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "WebDrivers/Chromedriver/chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
+    // Close the webdriver after we are done running the class
+    @AfterClass // close the driver after we are done testing
+    public void close() {
+        driver.close();
+    }
+
+    // ---------------------- Test Functions ------------------------------------
     @Test(priority = 1)
     void open_browser()
     {
